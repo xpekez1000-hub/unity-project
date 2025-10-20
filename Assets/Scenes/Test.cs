1c84abfcef5c26c777574ff56c70ba9611966909
TruongDo <xpekez1000@gmail.com>
2025-10-20 19:28:17 +0700

    modify script

--- Assets/Scenes/Test.cs
+++ Assets/Scenes/Test.cs
@@ -7,12 +7,8 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // dnovndvn
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
